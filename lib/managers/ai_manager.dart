8c1aed59ff5e8f7000636957cc0457fd26fc1c6e
Ana Carolina <cscarol20@gmail.com>
2024-05-19 19:15:37 -0300

    create a manager to handle AI request messages

---
+++ lib/managers/ai_manager.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+class AIManager {
+  late final GenerativeModel _model;
+  late final GenerativeModel _visionModel;
+  late final ChatSession _chat;
+
+  final safetySettings = [
+    SafetySetting(HarmCategory.harassment, HarmBlockThreshold.high),
+    SafetySetting(HarmCategory.hateSpeech, HarmBlockThreshold.high),
+  ];
+
+  static final AIManager shared = AIManager._privateConstructor();
+
+  AIManager._privateConstructor() {
+    onInit();
+  }
+
+  void onInit() {
+    var apiKey = dotenv.env['GEMINI_API_KEY'];
+    _model = GenerativeModel(
+      model: 'gemini-pro',
+      apiKey: apiKey ?? "",
+      generationConfig: GenerationConfig(maxOutputTokens: 100),
+      safetySettings: safetySettings
+    );
+    _visionModel = GenerativeModel(
+      model: 'gemini-pro-vision',
+      apiKey: apiKey ?? "",
+      generationConfig: GenerationConfig(maxOutputTokens: 100),
+      safetySettings: safetySettings
+    );
+    _chat = _model.startChat(history: [
+
+    ]);
+    print('init ai manager');
+
+  }
+
+  Future<Map<String, dynamic>> getAnswerFromAI(
+      {String? file, required String text}) async {
+    late final GenerateContentResponse response;
+
+      var tokens = await countToken(text);
+
+      if (tokens.totalTokens > 100) {
+        return {};
+      }
+
+    if (file != null) {
+      final firstImage = await (File(file).readAsBytes());
+      final prompt = TextPart(text);
+      final imageParts = [
+        DataPart('image/jpeg', firstImage),
+      ];
+
+      response = await _visionModel.generateContent([
+        Content.multi([prompt, ...imageParts])
+      ]);
+    } else {
+      var content = Content.text(text.toString());
+      response = await _chat.sendMessage(content);
+    }
+
+    return {
+      'time': DateTime.now(),
+      'message': response.text,
+      'isSender': false,
+      'isImage': false
+    };
+  }
+
+  Future<Iterable<Content>> getHistory() async {
+    var response = _chat.history.map((e) => e).toList();
+    return response;
+  }
+
+  Future<CountTokensResponse> countToken(String prompt) async {
+    final tokenCount = await _model.countTokens([Content.text(prompt)]);
+    print('Token count: ${tokenCount.totalTokens}');
+    return tokenCount;
+  }
+}
