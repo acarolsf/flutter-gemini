765c436d70737ac53f99306fb0cfd9f3da0db5d0
Ana Carolina <cscarol20@gmail.com>
2024-05-20 00:40:07 -0300

    read chat history

--- lib/screens/chat/chat_repository.dart
+++ lib/screens/chat/chat_repository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_gemini/managers/ai_manager.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 class ChatRepository {
 
@@ -7,5 +8,9 @@ class ChatRepository {
   Future<Map<String, dynamic>> getAnswerFromAI(
       {String? file, required String text}) async {
     return await AIManager.shared.getAnswerFromAI(file: file, text: text);
+  }
+
+  Future<List<Content>> getHistory() async {
+    return await AIManager.shared.getHistory().then((value) => value.toList());
   }
 }
